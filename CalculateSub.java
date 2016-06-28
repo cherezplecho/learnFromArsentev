cd71b7e7b9d53705527598c61636941a75d4a69a
cheresplecho <Yurik_16@tut.by>
2016-06-28 20:45:02 +0300

    Calc sub

---
+++ CalculateSub.java
@@ -0,0 +1,12 @@
+public class CalculateSub{
+	
+	public static void main(String[] arg) {
+		System.out.println("Calcutate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first - second;
+		System.out.println("Sum "+sum);
+
+	}
+
+}
